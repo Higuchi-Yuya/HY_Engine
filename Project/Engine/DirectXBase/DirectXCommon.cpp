0d31447ad9723cc7a137fb68fad155cb6e74f0cd
YuyaHiguchi <k021g1263@g.neec.ac.jp>
2023-10-17 16:41:40 +0900

    作ったブルーム関連のフィルターを整理

--- Project/Engine/DirectXBase/DirectXCommon.cpp
+++ Project/Engine/DirectXBase/DirectXCommon.cpp
@@ -180,7 +180,7 @@ void DirectXCommon::InitalizeDevice()
 	ComPtr<ID3D12Debug1> debugController;
 	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
 		debugController->EnableDebugLayer();
-		debugController->SetEnableGPUBasedValidation(true);
+		debugController->SetEnableGPUBasedValidation(false);
 	}
 #endif
 
